cad1241023b005171b943e099acdc488facec918
ethereal <ethereal_e@163.com>
2024-06-17 21:54:32 +0800

    2024.6.17

--- resource/ARM/ARM-LearningWhiteboard/drawingwidget.h
+++ resource/ARM/ARM-LearningWhiteboard/drawingwidget.h
@@ -23,6 +23,13 @@ public:
     void setPenColor(const QColor &color);
     void setPenWidth(int width);
 
+    // 设置多边形和函数图像参数的接口
+    void drawPolygon(const QVector<QPoint> &points);
+    void drawFunctionGraph(double (*func)(double), double xMin, double xMax, double step);
+
+    // 设置状态标志
+    void setMoveFlag(bool flag);
+
 protected:
     void mousePressEvent(QMouseEvent *event) override;
     void mouseMoveEvent(QMouseEvent *event) override;
@@ -31,10 +38,24 @@ protected:
 
 private:
     bool drawing = false;
+    bool moveFlag = false;  // 状态标志
     QPoint lastPoint;
     QVector<Line> lines;
-    QColor penColor = Qt::black;        //画笔颜色
-    int penWidth = 2;                   //画笔粗细
+    QVector<QPolygon> polygons;
+    QVector<QVector<QPointF>> functionGraphs;
+    QColor penColor = Qt::black;
+    int penWidth = 2;
+
+    // 框选和拖动相关变量
+    bool selecting = false;
+    bool dragging = false;
+    QRect selectionRect;
+    QPoint dragStartPos;
+    QVector<QPolygon> selectedPolygons;
+    QVector<QVector<QPointF>> selectedGraphs;
+
+    // 检查是否在框选区域内
+    bool isInSelectionArea(const QPoint &pos) const;
 };
 
 #endif // DRAWINGWIDGET_H
